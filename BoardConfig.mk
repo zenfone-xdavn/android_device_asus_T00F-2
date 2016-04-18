a2fcdedf278335a2b635569411e2d65ea378b1d5
artefvck <artefvck07@gmail.com>
2016-04-19 01:14:42 +0700

    T00F : minimalize device tree , migrate to common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@
 -include device/asus/cvtlp-common/BoardConfigCommon.mk
 
 #cmhw
-BOARD_HARDWARE_CLASS := device/asus/T00F/cmhw
+BOARD_HARDWARE_CLASS += device/asus/T00F/cmhw
 
 # Partitions
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
@@ -26,16 +26,6 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 5277466624 ##5277483008 - 16384 // 5033MB
 BOARD_CACHEIMAGE_PARTITION_SIZE := 519045120 # // 495MB
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1320157184 # // 1259MB
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Inline kernel building
-TARGET_KERNEL_SOURCE := kernel/asus/T00F
-TARGET_KERNEL_ARCH := x86
-BOARD_KERNEL_IMAGE_NAME := bzImage
-TARGET_KERNEL_CONFIG := cyanogenmod_T00F_defconfig
-
-# Kernel cmdline
-BOARD_KERNEL_CMDLINE := init=/init pci=noearly loglevel=0 kmemleak=off androidboot.bootmedia=sdcard vmalloc=256M androidboot.hardware=redhookbay watchdog.watchdog_thresh=60 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 snd_pcm.maximum_substreams=8 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on debug_locks=0
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/asus/T00F/sepolicy
